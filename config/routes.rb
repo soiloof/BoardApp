c4e365ee6747f918b37ea8262bc8c492be65e825
soiloof <tsucchy0718@gmail.com>
2018-12-09 13:28:41 +0900

    Improve Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/top'
   get 'pages/index'
+  get 'pages/newboard'
+  post '/pages/newboard', to: 'pages#createboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
